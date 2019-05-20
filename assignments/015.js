261f53232ea3493cf10c18ccdac7440a19b6332b
ahsanirfan85 <47439419+ahsanirfan85@users.noreply.github.com>
2019-05-20 15:19:27 -0600

    changes
    changes

--- assignments/015.js
+++ assignments/015.js
@@ -1,11 +1,16 @@
 // create secret number
-var secretNumber = Math.round(Math.random() * 10);
+// var secretNumber = Math.round(Math.random() * 10);
+var secretNumber = 5;
 
 // ask for number
-var guessString = prompt("Guess a number!");
+var guessString = prompt("Guess a number between 0 & 10!");
 var guessNum = Number(guessString);
-alert("You guessed: " + guessNum);
+
 // // check number against secret number
-// if (guessNum === secretNumber) {
-//   alert("Your guess is correct!");
-// }
+if (guessNum <= secretNumber) {
+  alert("Your guess is too low");
+} else if (guessNum === secretNumber) {
+  alert("You guessed correctly!");
+} else {
+  alert("Your guess is too high!");
+}
