9855530bf9c577430b8d4d9772f5162930959f58
ahsanirfan85 <47439419+ahsanirfan85@users.noreply.github.com>
2019-05-20 13:49:24 -0600

    changes
    changes

--- assignments/015.js
+++ assignments/015.js
@@ -3,4 +3,6 @@ if (age < 0) {
   alert("You cannot have an age less than zero!");
 } else if ((age = 21)) {
   alert("Happy 21st birthday!");
+} else if (age % 2 != 0) {
+  alert("Your age ," + age + " is an odd number!");
 }
