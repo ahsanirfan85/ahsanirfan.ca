c0fe838cf543115d89068bb9e6e1e6fdd5fdfdc3
ahsanirfan85 <47439419+ahsanirfan85@users.noreply.github.com>
2019-05-20 15:16:26 -0600

    changes
    changes

--- assignments/015.js
+++ assignments/015.js
@@ -1,10 +1,10 @@
 // create secret number
 var secretNumber = Math.round(Math.random() * 10);
-alert(Number(secretNumber));
 
 // ask for number
-// var guessString = prompt("Guess a number!");
-// var guessNum = Number(guessString);
+var guessString = prompt("Guess a number!");
+var guessNum = Number(guessString);
+alert("You guessed: " + guessNum);
 // // check number against secret number
 // if (guessNum === secretNumber) {
 //   alert("Your guess is correct!");
