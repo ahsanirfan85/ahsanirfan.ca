5e893be8bd023b0158f804862797300dd0a00d90
ahsanirfan85 <47439419+ahsanirfan85@users.noreply.github.com>
2019-05-20 14:09:34 -0600

    changes
    changes

--- assignments/015.js
+++ assignments/015.js
@@ -4,10 +4,10 @@ if (age < 0) {
   alert("You cannot have an age less than zero!");
 } else if (age <= 21) {
   alert("Happy 21st birthday!");
-} else if (age % 2 !== 0) {
-  alert("Your age, " + age + ", is an odd number!");
 } else if (age % math.sqrt(age) === 0) {
   alert("Your age is a perfect square!");
+} else if (age % 2 !== 0) {
+  alert("Your age, " + age + ", is an odd number!");
 } else
   alert(
     "You don't have a negative age, you're not 21, you don't have an odd age and your age is not a perfect square!"
