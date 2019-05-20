419156567720fd9e4bab7a78b04d716b76bdb3ad
ahsanirfan85 <47439419+ahsanirfan85@users.noreply.github.com>
2019-05-20 14:02:46 -0600

    ahsan
    ahsan

--- assignments/015.js
+++ assignments/015.js
@@ -1,10 +1,13 @@
 var age = prompt("What is your age in years?");
 if (age < 0) {
   alert("You cannot have an age less than zero!");
-} else if (age === 21) {
+}
+if (age === 21) {
   alert("Happy 21st birthday!");
-} else if (age % 2 !== 0) {
+}
+if (age % 2 !== 0) {
   alert("Your age, " + age + ", is an odd number!");
-} else ((age % math.sqrt(age)) === 0) {
-  alert("Your age is a perfect square!")
+}
+if (age % math.sqrt(age) === 0) {
+  alert("Your age is a perfect square!");
 }
