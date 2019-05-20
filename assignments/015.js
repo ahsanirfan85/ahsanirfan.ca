c6fbb227bc15eff1bc4c75f00983198f038cbc85
ahsanirfan85 <47439419+ahsanirfan85@users.noreply.github.com>
2019-05-20 13:54:35 -0600

    changes
    changes

--- assignments/015.js
+++ assignments/015.js
@@ -1,7 +1,7 @@
 var age = prompt("What is your age in years?");
 if (age < 0) {
   alert("You cannot have an age less than zero!");
-} else if ((age = 21)) {
+} else if (age <= 21) {
   alert("Happy 21st birthday!");
 } else if (age % 2 != 0) {
   alert("Your age, " + age + ", is an odd number!");
