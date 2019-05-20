3e17304dfc5ed9f9a5e22c61109f109db5adfa0d
ahsanirfan85 <47439419+ahsanirfan85@users.noreply.github.com>
2019-05-20 11:42:54 -0600

    changes
    changes

---
+++ assignments/013.js
@@ -0,0 +1,7 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+fullName = firstName + " " + lastName;
+var age = prompt("How old are you?");
+
+console.log("Your full name is " + fullName + ".");
+console.log("You are " + age + " years old.");
