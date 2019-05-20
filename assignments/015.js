769b056e6e0a261161a997b4af09aacf83851186
ahsanirfan85 <47439419+ahsanirfan85@users.noreply.github.com>
2019-05-20 14:54:29 -0600

    changes
    changes

--- assignments/015.js
+++ assignments/015.js
@@ -1,9 +1,10 @@
 // create secret number
 // var randNum = math.random();
 // var randNumTen = randNum * 10;
+var unrounded = 30.445;
 
 var secretNum = 300;
-alert("The secret number is: " + secretNum);
+alert("The secret number is: " + unrounded);
 
 // ask for number
 
