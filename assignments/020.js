61a50b011da22717403a81f804b8d3140762ac3c
ahsanirfan85 <47439419+ahsanirfan85@users.noreply.github.com>
2019-05-21 12:37:06 -0600

    changes
    changes

--- assignments/020.js
+++ assignments/020.js
@@ -2,4 +2,7 @@ var todos = ["Buy new turtle"];
 var input = prompt("What would you like to do?");
 if (input === "list") {
   console.log(todos);
+} else if (input === "new") {
+  var new Todo = prompt("Enter new todo");
+  todos.push(newTodo);
 }
