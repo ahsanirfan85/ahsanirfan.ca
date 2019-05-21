fcfbc896e0ce4bbb1a1ab6aa81ccf5f5bbafc4b8
ahsanirfan85 <47439419+ahsanirfan85@users.noreply.github.com>
2019-05-20 21:19:11 -0600

    changes
    changes

--- assignments/016.js
+++ assignments/016.js
@@ -1,7 +1,19 @@
 //print all numbers between -10 & 19
+console.log("PRINT ALL NUMBERS BETWEEN -10 & 19");
 var allNumbers = -10;
 
 while (allNumbers < 20) {
   console.log(allNumbers);
   allNumbers++;
+}
+
+//print all even numbers between 10 & 40
+console.log("PRINT ALL EVEN NUMBERS BETWEEN 10 & 40");
+var evenNumbers = 10;
+
+while (evenNumbers <= 40) {
+  if (evenNumbers % 2 === 0) {
+    console.log(evenNumbers);
+    evenNumbers++;
+  }
 }
