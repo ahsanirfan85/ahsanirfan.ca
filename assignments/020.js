edffdcf616d78ee4cf642afff3cf0503d0dbd147
ahsanirfan85 <47439419+ahsanirfan85@users.noreply.github.com>
2019-05-21 12:45:48 -0600

    changes
    changes

--- assignments/020.js
+++ assignments/020.js
@@ -1,11 +1,12 @@
 var todos = ["Buy new turtle"];
 var input = prompt("What would you like to do?");
-while (input != "quit") {
+while (input !== "quit") {
   if (input === "list") {
     console.log(todos);
   } else if (input === "new") {
     var newTodo = prompt("Enter new todo");
     todos.push(newTodo);
   }
+  input = prompt("What would you like to do?");
 }
 console.log("OK YOU QUIT THE APP");
