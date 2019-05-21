ccbf77b744d89e59b5c617913e9da0c38298ec14
ahsanirfan85 <47439419+ahsanirfan85@users.noreply.github.com>
2019-05-20 22:45:37 -0600

    ahsan
    ahsan

--- assignments/018.js
+++ assignments/018.js
@@ -1,12 +1,12 @@
 //print all numbers between -10 & 19
 console.log("PRINT ALL NUMBERS BETWEEN -10 & 19");
-for (i=-10,i < 20,i++) {
+for (var i = -10; i < 20; i++) {
   console.log(i);
 }
 
 //print all even numbers between 10 & 40
 console.log("PRINT ALL EVEN NUMBERS BETWEEN 10 & 40");
-for (j=10,j <= 40,j++) {
+for (var j = 10; j <= 40; j++) {
   if (j % 2 === 0) {
     console.log(j);
   }
@@ -14,7 +14,7 @@ for (j=10,j <= 40,j++) {
 
 //print all odd numbers between 300 & 333
 console.log("PRINT ALL ODD NUMBERS BETWEEN 300 & 333");
-for (k=300,k <= 333,k++) {
+for (var k = 300; k <= 333; k++) {
   if (k % 2 === 1) {
     console.log(k);
   }
@@ -22,7 +22,7 @@ for (k=300,k <= 333,k++) {
 
 //print all numbers divisible by 5 AND 3 between 5 & 50
 console.log("PRINT ALL NUMBERS DIVISIBLE BY 5 AND 3 BETWEEN 5 AND 50");
-for (l=5,l <= 50,l++) {
+for (var l = 5; l <= 50; l++) {
   if (l % 15 === 0) {
     console.log(l);
   }
